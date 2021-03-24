c43c31f5d2b4c89b0c1fe9589235464bef4ffd91
Mathias Sundholm <mathias_sundholm@hotmail.com>
2021-03-24 07:58:26 +0200

    moved files to src

---
+++ src/building.py
@@ -0,0 +1,147 @@
+#import optuna
+import yaml
+import random
+from copy import deepcopy
+#def objective(trial):
+#    x = trial.suggest_uniform('x', -10, 10)
+#    return (x - 2) ** 2
+
+#study = optuna.create_study()
+#study.optimize(objective, n_trials=100)
+
+#study.best_params  # E.g. {'x': 2.002108042}
+
+class BuildingDeck():
+    def __init__(self):
+
+        with open("building_cards.yaml") as f:
+            card_data = yaml.safe_load(f)
+
+        deck = []
+        for card in card_data["deck"]:
+            deck += [card["card"]]*card["num"] 
+
+        cards = {x["name"]: x for x in card_data["card"]}
+        self.cards = [BuildingsCard(**deepcopy(cards[x])) for x in deck]
+
+        resource_list = set()
+        for card in self.cards:
+            #import pdb; pdb.set_trace()
+            for production_option in card.production:
+                for item in production_option["result"]:
+                    resource_list.add(item)
+        self.all_resource_list = list(resource_list)
+
+    def draw_card(self):
+        return self.cards.pop()
+    
+    def shuffle_(self):
+        random.shuffle(self.cards)
+
+class BuildingsCard():
+    def __init__(self, name, description, cost, production, victory_points):
+        self.name = name
+        self.description = description
+        self.cost = self.parse_item_cost(cost)
+        print(self.name, production)
+        self.production = production
+        for idx, production_option in enumerate(self.production):
+            self.production[idx]["cost"] = self.parse_item_cost(production_option.get("cost", []))
+            self.production[idx]["result"] = self.parse_item_cost(production_option.get("result",[])) 
+           
+        self.victory_points = victory_points
+
+        
+
+    def parse_item_cost(self, item_list):
+        print("item",item_list)
+        # convert ["2 grain", "4 stone"] into {"grain": 2, "stone": 4}
+        return {x.split(" ")[1]: float(x.split(" ")[0]) for x in item_list}
+
+
+    def can_build(self, inventory):
+        # Return true if inventory is available to build building
+        for item, cost in self.cost.items():
+            if item not in inventory.keys():
+                # item not available
+                return False 
+            else:
+                if cost > inventory[item]:
+                    # item available but not enough
+                    return False 
+        # item available and enough to build   
+        return True
+    
+        
+    def __repr__(self):
+        return "<BuildingsCard: {}>".format(self.name)
+
+
+from collections import Counter
+
+if __name__ == "__main__":
+    deck = BuildingDeck()
+    print(deck.all_resource_list)
+
+    card_dict = dict()
+    card_counter = Counter([x.name for x in deck.cards])
+
+
+    #relative_value_items = avg_production/game / total_production/game
+    #max_production/game = num_producer_cards*num_items_per_producer
+    #max_
+
+    # theorietical max items that can be produces in one round
+    max_production_dict = dict()
+    for resource in deck.all_resource_list:
+            max_production_per_round = 0
+            for key in card_counter.keys():
+                card_instance = next(x for x in deck.cards if x.name == key)
+                for production_option in card_instance.production:
+                    #import pdb; pdb.set_trace()
+                    max_items_per_producer = production_option["result"].get(resource, 0)
+                    max_production_per_round += card_counter[key] * max_items_per_producer
+                    max_production_dict[resource] = max_production_per_round
+    
+    print("max_production_per_round",max_production_dict)
+
+     # theorietical max items that can be consumed in one round
+    max_consumption_dict = dict()
+    for resource in deck.all_resource_list:
+            max_consumpion_per_round = 0
+            for key in card_counter.keys():
+                card_instance = next(x for x in deck.cards if x.name == key)
+                # building cost
+                building_cost = card_instance.cost.get(resource, 0)
+                max_consumpion_per_round += card_counter[key] * building_cost
+                for production_option in card_instance.production:
+                    # production cost
+                    max_items_per_producer = production_option["cost"].get(resource, 0)
+                    max_consumpion_per_round += card_counter[key] * max_items_per_producer
+                    
+                    max_consumption_dict[resource] = max_consumpion_per_round
+    
+    print("max_consumpion_per_round",max_consumption_dict)
+
+    for key in max_consumption_dict.keys():
+        value = max_consumption_dict[key]/max_production_dict[key]
+        print(key, value)
+
+
+
+
+# ==============================
+# Unit tests
+def test_building_can_buld():
+    card = BuildingsCard(name="", description="", cost=["5 wood", "3 grain"], production={}, victory_points=0)
+    inventory = {"wood": 5, "grain": 3}
+    assert card.can_build(inventory) == True
+    inventory = {"wood": 5, "grain": 2}
+    assert card.can_build(inventory) == False
+
+def test_deck_draw_card():
+    deck = BuildingDeck()
+    num_cards = len(deck.cards)
+    card = deck.draw_card()
+    assert len(deck.cards) == num_cards-1
+    
---
+++ src/player.py
@@ -0,0 +1,151 @@
+import random
+from copy import copy
+
+class Player:
+    def __init__(self, player_id):
+        self.player_id = player_id
+        self.market = {} # {item: price}
+        self.inventory = {} # {item: amount}
+        self.merchant_pos = "Home"
+        self.money = 50
+        self.victory_points = 0
+        self.buildings = []
+        self.building_card_hand = []
+
+        # bookeeping history
+        self.player_inventory_history = list()
+        self.player_money_history = list()
+  
+    def log(self, log_string):
+        print("Player {}: {}".format(self.player_id, log_string))
+ 
+    def buy(self, item, amount, other_player):
+        
+        cost = other_player.market[item]*amount
+       
+        if amount > other_player.inventory[item]:
+            raise "Tried to buy {} items, but only {} available".format(self.money,cost)
+            return
+
+        if self.money < cost:
+            raise "Tried to buy {} {} items. Not enough money: {} < {}".format(amount, item,self.money,cost)
+            return
+            
+        # make transaction
+        self.money -= cost
+        other_player.money += cost
+        other_player.inventory[item] -= amount
+        self.inventory[item] += amount
+        self.log("Buying: {} {}'s for {} from player {}".format(amount, item, cost, other_player.player_id))
+        
+        
+    def set_player_refs(self, other_players_ref, building_deck_ref):
+        # get latest info about other player
+        self.other_players_ref = other_players_ref
+        self.building_deck_ref = building_deck_ref
+
+    def init_inventory(self,start_inventory,building_deck):
+        # init inventory
+        all_resources = building_deck.all_resource_list
+        self.inventory = dict()
+        for item in all_resources:
+            self.inventory[item] = 0
+        for key, value in start_inventory.items():
+            self.inventory[key] = value
+
+
+    def draw_start_cards(self, building_deck):
+        for i in range(5):
+            card = building_deck.draw_card()
+            self.building_card_hand.append(card)
+
+    def adjust_market(self):
+        self.log("adjust market")
+        # Player adjusts market price of its inventory items
+        self.market = {}
+        for item, amount in self.inventory.items():
+            if amount > 0:
+                price = random.randint(5, 20)
+                self.market[item] = price
+        self.log(self.market)
+        self.log(self.inventory)
+        
+    def trade(self):
+        self.log("trade")
+        # Player select a fellow merchant player to trade with
+        fellow_merchant = random.choice(self.other_players_ref)
+        self.log("Choosing player {} to trade with".format(fellow_merchant.player_id))
+        
+        can_afford_items = [x for x,y in fellow_merchant.market.items() if y <= self.money]
+        if len(can_afford_items) > 0:
+            item = random.choice(can_afford_items)
+            price = fellow_merchant.market[item]
+            max_amount = self.money // price
+            max_amount = min(max_amount,fellow_merchant.inventory[item])
+            amount = random.randint(0, max_amount)
+            self.buy(item, amount, fellow_merchant)
+            
+        return None
+
+    def build(self):
+        print("player {}: build".format(self.player_id))
+        # build building action
+        can_build_buildings = [(i,x) for i, x in enumerate(self.building_card_hand) if x.can_build(self.inventory)]
+        if len(can_build_buildings) > 0:
+            to_build_choice = random.choice(can_build_buildings)
+            to_build_idx = to_build_choice[0]
+            to_build = to_build_choice[1]
+            # pay building cost
+            for item, cost in to_build.cost.items():
+                self.inventory[item] -= cost
+            print("player {}: building".format(self.player_id,to_build))
+            # build
+            self.buildings.append(to_build)
+            self.building_card_hand.pop(to_build_idx)
+        # change building card action
+        if len(self.building_card_hand) > 0: 
+            to_change_idx = random.randint(0,len(self.building_card_hand)-1)
+            self.building_card_hand.pop(to_change_idx)
+
+        # TODO discard pile
+
+        # draw cards to fill hand
+        while self.building_card_hand < 5:
+            card = building_deck.draw_card()
+            self.building_card_hand.append(card)
+
+        return None
+
+    def collect(self):
+        print("player {}: collect".format(self.player_id))
+
+        # collect resources
+        for building in self.buildings:
+            for production_option in building.production:
+                
+                # TODO choose what to produce
+
+                # pay production cost
+                for item_name, item_amount in production_option["cost"].items():
+                    if self.inventory[item_name] - item_amount >= 0: 
+                        self.inventory[item_name] -= item_amount
+                    else:
+                        continue
+                # get produced items
+                for item_name, item_amount in production_option["result"].items():    
+                    self.inventory[item_name] += item_amount
+        return None
+
+    def log_data(self):
+            self.player_inventory_history.append(copy(self.inventory))
+            self.player_money_history.append(copy(self.money))
+    
+
+
+# ==============================
+# Unit tests
+
+def test_player_buy():
+    players = [Player(player_id=x) for x in range(2)]
+    builidng_deck = BuildingDeck()
+    
